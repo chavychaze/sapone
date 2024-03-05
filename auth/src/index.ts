1d365b43ebc6e05c10d5db2236cce2b35d4beb06
Vitaliy Sobol <vitaliy@2solar.nl>
2024-03-05 14:56:31 +0200

    add 404 error handling; add async express package to avoid use next()

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/users/current-user';
@@ -6,6 +7,7 @@ import { signinRouter } from './routes/users/signin';
 import { signoutRouter } from './routes/users/signout';
 import { signupRouter } from './routes/users/signup';
 import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -14,6 +16,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
