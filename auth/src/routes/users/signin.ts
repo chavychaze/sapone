d5fe06157d3d252ecd79ae9486815137eacf7a5c
Vitaliy Sobol <vitaliy@2solar.nl>
2024-03-06 16:37:45 +0200

    add body validation middleware

--- auth/src/routes/users/signin.ts
+++ auth/src/routes/users/signin.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../../middleware/validate-request';
+import { User } from '../../models/user';
+import { BadRequestError } from '../../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('You must apply a password'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/users/signup.ts
+++ auth/src/routes/users/signup.ts
@@ -1,54 +1,46 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import jwt from 'jsonwebtoken';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
-import { User } from '../../models/user';
-import { RequestValidationError } from '../../errors/request-validation-errors';
-import { BadRequestError } from '../../errors/bad-request-error';
+import { validateRequest } from "../../middleware/validate-request";
+import { User } from "../../models/user";
+import { BadRequestError } from "../../errors/bad-request-error";
 
 const router = express.Router();
 
 router.post(
-    '/api/users/signup',
-    [
-        body('email').isEmail().withMessage('Email must be valid.'),
-        body('password')
-            .trim()
-            .isLength({ min: 4, max: 20 })
-            .withMessage('Password must be between 4 and 20 characters.'),
-    ],
-    async (req: Request, res: Response) => {
-        const errors = validationResult(req);
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid."),
+    body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters."),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
+    const existingUser = await User.findOne({ email });
 
-        const { email, password } = req.body;
-
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-            throw new BadRequestError('Email already in use.');
-        }
-
-        const user = User.build({ email, password });
-        await user.save();
-
-        const userJwt = jwt.sign(
-            {
-                id: user.id,
-                email: user.email,
-            },
-            process.env.JWT_KEY!
-        );
-
-        req.session = {
-            jwt: userJwt,
-        };
-
-        res.status(201).send(user);
+    if (existingUser) {
+      throw new BadRequestError("Email already in use.");
     }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(user);
+  },
 );
 
 export { router as signupRouter };
