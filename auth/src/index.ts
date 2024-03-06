d2dc88851c0b9febd88a5fc76a834ae6618dc4e3
Vitaliy Sobol <vitaliy@2solar.nl>
2024-03-06 16:19:13 +0200

    add cookies and jwt; update retrieve user data in response

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/users/current-user';
 import { signinRouter } from './routes/users/signin';
@@ -11,7 +12,14 @@ import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,6 +33,10 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT must be defined');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log('Connected to MongoDB.');
--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -6,8 +6,8 @@ import { Password } from '../services/password';
  * that are required to create a new User
  */
 interface UserAttrs {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 /**
@@ -15,7 +15,7 @@ interface UserAttrs {
  * that a User Model has
  */
 interface UserModel extends mongoose.Model<UserDoc> {
-    build(attrs: UserAttrs): UserDoc;
+  build(attrs: UserAttrs): UserDoc;
 }
 
 /**
@@ -23,31 +23,43 @@ interface UserModel extends mongoose.Model<UserDoc> {
  * that a User Document has
  */
 interface UserDoc extends mongoose.Document {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-});
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
-    if (this.isModified('password')) {
-        const hashed = await Password.toHash(this.get('password'));
-        this.set('password', hashed);
-    }
-    done();
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
-    return new User(attrs);
+  return new User(attrs);
 };
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
