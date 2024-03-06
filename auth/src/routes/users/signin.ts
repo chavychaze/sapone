905fdcbf5baefc27cbe7511eb5f0dc05776786cd
Vitaliy Sobol <vitaliy@2solar.nl>
2024-03-06 16:44:05 +0200

    finish with signin

--- auth/src/routes/users/signin.ts
+++ auth/src/routes/users/signin.ts
@@ -4,20 +4,42 @@ import jwt from 'jsonwebtoken';
 
 import { validateRequest } from '../../middleware/validate-request';
 import { User } from '../../models/user';
+import { Password } from '../../services/password';
 import { BadRequestError } from '../../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
   '/api/users/signin',
-  [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().notEmpty().withMessage('You must apply a password'),
-  ],
+  [body('email').isEmail().withMessage('Email must be valid'), body('password').trim().notEmpty().withMessage('You must apply a password')],
   validateRequest,
-  (req: Request, res: Response) => {
-    
-  }
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  },
 );
 
 export { router as signinRouter };
--- auth/src/routes/users/signup.ts
+++ auth/src/routes/users/signup.ts
@@ -1,18 +1,18 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import jwt from "jsonwebtoken";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
-import { validateRequest } from "../../middleware/validate-request";
-import { User } from "../../models/user";
-import { BadRequestError } from "../../errors/bad-request-error";
+import { validateRequest } from '../../middleware/validate-request';
+import { User } from '../../models/user';
+import { BadRequestError } from '../../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signup",
+  '/api/users/signup',
   [
-    body("email").isEmail().withMessage("Email must be valid."),
-    body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters."),
+    body('email').isEmail().withMessage('Email must be valid.'),
+    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters.'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -21,7 +21,7 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError("Email already in use.");
+      throw new BadRequestError('Email already in use.');
     }
 
     const user = User.build({ email, password });
