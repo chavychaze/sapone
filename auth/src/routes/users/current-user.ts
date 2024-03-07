320b7793426968ba8764252cc353beccd22fddb3
Vitaliy Sobol <vitaliy@2solar.nl>
2024-03-07 18:27:13 +0200

    finish with authorization; start with testing

--- auth/src/routes/users/current-user.ts
+++ auth/src/routes/users/current-user.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
+import { currentUser } from '../../middleware/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
