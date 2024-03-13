ab81e3c261099a15de573827292e7ad12a18e212
Vitaliy Sobol <vitaliy@2solar.nl>
2024-03-13 16:30:45 +0200

    update ingress-nginix file and add client settings to it; start with client; add landing page, signin and singup pages; add handling cookies; start with passing a initial Props in each components

---
+++ client/pages/_app.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default AppComponent;
