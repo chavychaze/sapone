b1ffd250cdd01abd8f4cb1e55f57b05f74952836
Vitaliy Sobol <vitaliy@2solar.nl>
2024-04-09 16:33:17 +0300

    update common typo; add publish for update route

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -8,6 +8,8 @@ import {
 } from '@zibelina/common';
 
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -29,15 +31,22 @@ router.put(
       throw new NotFoundError();
     }
 
-    if(ticket.userId !== req.currentUser!.id){
+    if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
 
     const { title, price } = req.body;
     ticket.set({
-      title, price
+      title,
+      price,
     });
     await ticket.save();
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   },
